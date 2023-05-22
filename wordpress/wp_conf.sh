1a46897d05af02c18c834ec7a5ad5c74fc58fa8a
boo <boo@ubntu.myguest.virtualbox.org>
2023-05-22 22:18:13 +0400

    wpwp *v*

--- wordpress/wp_conf.sh
+++ wordpress/wp_conf.sh
@@ -8,32 +8,19 @@ DB_HOST="localhost"
 
 apt-get install -y wget && wget -P /var/www/html/ https://wordpress.org/latest.tar.gz && tar -xvf /var/www/html/latest.tar.gz -C /var/www/html/
 
+wp_conf_path="/var/www/html/wordpress/wp-config.php"
+
 touch /var/www/html/boo
 
-# cd /var/www/html/wordpress/
+if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
 
-
-# if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
-# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php; 
-
-if [ ! -e /var/www/html/wp-config.php ]; then
-
-cp -r /wordpress/* /var/www/html/
-
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-wp-conf-path="/var/www/html/wordpress/wp-config.php"
-# Wait for the database container to be ready
-# until mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e 'show databases'; do
-# 	>&2 echo "MySQL is unavailable - sleeping"
-# 	sleep 1
-# done
+cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
 #update wp-config.php
-sed -i "s/database_name_here/$DB_NAME/" ${wp-conf-path}
-sed -i "s/username_here/$DB_USER/" ${wp-conf-path}
-sed -i "s/password_here/$DB_PASSWORD/" ${wp-conf-path}
-sed -i "s/localhost/$DB_HOST/" ${wp-conf-path}
+sed -i "s/database_name_here/$DB_NAME/" ${wp_conf_path}
+sed -i "s/username_here/$DB_USER/" ${wp_conf_path}
+sed -i "s/password_here/$DB_PASSWORD/" ${wp_conf_path}
+sed -i "s/localhost/$DB_HOST/" ${wp_conf_path}
 fi
 chown -R www-data:www-data /var/www/html/wordpress && chmod -R 755 /var/www/html/wordpress
 tail -f /dev/null
