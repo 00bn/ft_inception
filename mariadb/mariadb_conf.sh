d338ce700c8f0fbb9ea375e887f9b5a9a261fe95
nahdib0@gmail.com <nahdib0@gmail.com>
2023-06-14 23:18:16 +0400

    Done maybe

---
+++ mariadb/mariadb_conf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Start MariaDB server
+sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+sed -i 's/^#port/port/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+service mysql start
+
+mysql -e "CREATE DATABASE wpdb;"
+mysql -e "CREATE USER 'wpuser'@'%' identified by 'dbpassword';"
+mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+service mysql stop
+
+mysqld --console
+
+
--- wordpress/wp_conf.sh
+++ wordpress/wp_conf.sh
@@ -4,28 +4,29 @@
 DB_NAME="wpdb"
 DB_USER="wpuser"
 DB_PASSWORD="dbpassword"
-DB_HOST="localhost"
+DB_HOST="mariadb"
 
 apt-get install -y wget && wget -P /var/www/html/ https://wordpress.org/latest.tar.gz && tar -xvf /var/www/html/latest.tar.gz
 
-# touch /var/www/html/boo
+cp -r /wordpress/* /var/www/html/
 
-# cd /var/www/html/wordpress/
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-# if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
-# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php; 
+chmod +x wp-cli.phar
 
-# if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
+mv wp-cli.phar /usr/local/bin/wp
 
-# cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-cp /wordpress/wp-config-sample.php /wordpress/wp-config.php
+#installation page 
+wp core install --allow-root --path=/var/www/html/ --url=localhost --title="YOOO IT'S B O O" --admin_user="wpuser" --admin_password="dbpassword" --admin_email="oobn4444@gmail.com"
+echo "BOO: wp installation page !!"
+#user creation
+wp user create --allow-root --path=/var/www/html/ "wp_boo" "nahdib0@gmail.com" --role=contributor --user_pass=s"boopassword"
+echo "BOO: create user in wp db !!"
 
-wp="/wordpress/wp-config.php"
-# Wait for the database container to be ready
-# until mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e 'show databases'; do
-# 	>&2 echo "MySQL is unavailable - sleeping"
-# 	sleep 1
-# done
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+
+wp="/var/www/html/wp-config.php"
 
 #update wp-config.php
 sed -i "s/database_name_here/$DB_NAME/" ${wp}
@@ -33,5 +34,11 @@ sed -i "s/username_here/$DB_USER/" ${wp}
 sed -i "s/password_here/$DB_PASSWORD/" ${wp}
 sed -i "s/localhost/$DB_HOST/" ${wp}
 
-chown -R www-data:www-data /wordpress && chmod -R 755 /wordpress
-tail -f /dev/null
+#instlation page 
+
+chown -R www-data:www-data /var/www/html/
+
+sed -i 's/listen = .*/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+php-fpm7.3 -F
+
