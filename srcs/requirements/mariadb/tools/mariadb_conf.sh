181002ada7d173baa7c11138fcbfaa27030e226d
nahdib0@gmail.com <nahdib0@gmail.com>
2023-07-02 09:42:45 +0400

    +_+

--- srcs/requirements/mariadb/tools/mariadb_conf.sh
+++ srcs/requirements/mariadb/tools/mariadb_conf.sh
@@ -8,11 +8,15 @@ sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d
 sed -i 's/^#port/port/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
-
+if [! -d "/var/lib/mysql/$DB_NAME"]; then
 mysql -e "CREATE DATABASE $DB_NAME ;"
 mysql -e "CREATE USER '$DB_USER'@'%' identified by '$DB_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
+echo "Database created"
+else 
+echo "Database already exists"
+fi
 
 service mysql stop
 
